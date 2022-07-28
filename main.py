a194009703dd4d2f66559376d388924d2fc7f06e
BadBoy457 <108492728+BadBoy457@users.noreply.github.com>
2022-07-29 02:02:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,9 +56,9 @@ async def start_handler(bot: Client, m: Message):
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                 InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
+                [InlineKeyboardButton("Bot Owner - @babyiambadboy", url="https://t.me/babyiambadboy")],
+                [InlineKeyboardButton("Support Group", url="https://t.me/bot88updates"),
+                 InlineKeyboardButton("Video Channel", url="https://t.me/hhtjhjmfmfm")],
                 [InlineKeyboardButton("Open Settings", callback_data="openSettings")],
                 [InlineKeyboardButton("Close", callback_data="closeMeh")]
             ]
@@ -363,7 +363,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         await cb.message.edit(
             text=Config.START_TEXT,
             parse_mode="Markdown",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/linux_repo")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer - @babyiambadboy", url="https://t.me/babyiambbadboy"), InlineKeyboardButton("Support Group", url="https://t.me/bot88updates")], [InlineKeyboardButton("Video Channel", url="https://t.me/hhtjhjmfmfm")]]),
             disable_web_page_preview=True
         )
     elif "showThumbnail" in cb.data:
